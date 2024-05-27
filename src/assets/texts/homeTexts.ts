230815e367125e2f1277a2ad63f0149ea0acddef
Luana Reginato Bassanesi <luana.bassanesi@develcode.com.br>
2024-05-27 11:02:06 -0300

    assets & styles

---
+++ src/assets/texts/homeTexts.ts
@@ -0,0 +1,8 @@
+export const H1 = "Desenvolvendo uma web acessível";
+export const H2 =
+  "Protocolos e diretrizes orientam o desenvolvimento de tecnologias acesíveis, mas é preciso olhar para além de tudo isso";
+export const H3 = "O que é acessibilidade afinal?";
+export const P1 =
+  "A acessibilidade se tornou uma tendênca no ecossistema tecnológico mundial, diversas empresas passaram a adotar critérios de desenvolvimento acessível em seus projetos - por uma questão prática ou por exigências legais -, no entanto, ainda faltam recursos e consciência do que de fato é inclusivo na internet para pessoas com deficiências.";
+export const P2 =
+  "Para abordar este tema, é preciso ter uma perspectiva cultural soobre o assunto, não encará-lo apenas na ótica técnica ou prática. Acessibilidade passou a ser vista como um desafio (ou tendência) para o ecossistema tech e não como filosofia natural daquilo que condiz com os princípios básicos da web: acessível para humanos. Tal afirmação está na W3C";
